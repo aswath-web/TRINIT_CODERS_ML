5a29c17bdccda9efceaf7dd77007ca3ca5ec270d
Aswath AR <73185057+aswath-web@users.noreply.github.com>
2023-02-11 23:46:52 +0530

    Add files via upload

---
+++ home/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+import pickle
+
+def home(request):
+
+    return render(request,'home.html')
+
+def predict(request):
+    if request.method == 'POST':
+        n=float(request.POST.get('n'))
+        p=float(request.POST.get('p'))
+        k=float(request.POST.get('k'))
+        temp=float(request.POST.get('temp'))
+        humi=float(request.POST.get('humi'))
+        ph=float(request.POST.get('ph'))
+        rain=float(request.POST.get('rain'))
+        random = pickle.load(open('random_forest.sav','rb'))
+        result = random.predict([[n,p,k,temp,humi,ph,rain]])
+        crop=result[0]
+        return render(request,'result.html',{'crr':crop})
